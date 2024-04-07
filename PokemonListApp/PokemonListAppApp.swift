a088a62d110f327ebe81183dc693058c3dc58b35
Can Kaynar <cannkaynarr@outlook.com>
2024-04-07 19:58:43 +0300

    Initial Commit

---
+++ PokemonListApp/PokemonListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonListAppApp.swift
+//  PokemonListApp
+//
+//  Created by Can on 7.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
